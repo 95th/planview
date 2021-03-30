7c82e092a1529565d1b2916c9699b9db19b1eef0
Gurwinder Singh <vargwin@gmail.com>
2021-03-30 13:37:30 +0530

    API logging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HTTP_INTERCEPTORS_PROVIDER } from './http-interceptors';
 import { ImportsModule } from './imports/imports.module';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
@@ -12,7 +13,7 @@ import { RegistrationComponent } from './registration/registration.component';
 @NgModule({
     declarations: [AppComponent, RegistrationComponent, HomeComponent, LoginComponent],
     imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, ImportsModule],
-    providers: [],
+    providers: [HTTP_INTERCEPTORS_PROVIDER],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/services/logging.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+
+interface LogData {
+    id: number;
+    user_id: string;
+    url: string;
+    url_with_query?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class LoggingService {
+    constructor(private http: HttpClient, private auth: AuthService) {}
+
+    async log_request(url: string, url_with_query?: string) {
+        const data: LogData = {
+            id: 0,
+            user_id: this.auth.username,
+            url,
+            url_with_query,
+        };
+        await this.http.post('/api/api-log', data).toPromise();
+    }
+}
