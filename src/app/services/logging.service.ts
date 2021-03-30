e6485d2065b6f667c2e3e6d65d07da5fb998fe26
Gurwinder Singh <vargwin@gmail.com>
2021-03-30 13:41:22 +0530

    log with params only if needed

--- src/app/services/logging.service.ts
+++ src/app/services/logging.service.ts
@@ -2,23 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
-interface LogData {
+interface RequestLogData {
     id: number;
     user_id: string;
     url: string;
     url_with_query?: string;
+    timestamp: string;
 }
 
 @Injectable({ providedIn: 'root' })
 export class LoggingService {
     constructor(private http: HttpClient, private auth: AuthService) {}
 
-    async log_request(url: string, url_with_query?: string) {
-        const data: LogData = {
+    async log_request(url: string, url_with_query: string) {
+        const data: RequestLogData = {
             id: 0,
             user_id: this.auth.username,
             url,
-            url_with_query,
+            url_with_query: url === url_with_query ? undefined : url_with_query,
+            timestamp: new Date().toISOString(),
         };
         await this.http.post('/api/api-log', data).toPromise();
     }
