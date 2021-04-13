b497804186eaae124ce158c69f5575d5612140de
Gurwinder Singh <vargwin@gmail.com>
2021-04-13 22:16:29 +0530

    fmt

--- apps/planv/src/app/util/date.util.spec.ts
+++ apps/planv/src/app/util/date.util.spec.ts
@@ -2,7 +2,7 @@ import { dateToString } from './date.util';
 
 describe('date util', () => {
     it('should create ISO string', () => {
-        const date = new Date(Date.parse("2020-01-04"));
+        const date = new Date(Date.parse('2020-01-04'));
         const str = dateToString(date);
         expect(str).toStrictEqual('2020-01-04');
     });
